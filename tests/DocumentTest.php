77a2a9bfff12d0ef32269e520bb730ad62b77c6b
Vinicius Dutra <dutra_morais@hotmail.com>
2020-03-07 01:35:02 -0300

    travis implement with test phpunit

--- tests/DocumentTest.php
+++ tests/DocumentTest.php
@@ -2,13 +2,14 @@
 
 use PHPUnit\Framework\TestCase;
 use vinicinbgs\Autentique\Documents;
+use Dotenv\Dotenv;
 
 final class DocumentTest extends TestCase
 {
     public function __construct(?string $name = null, array $data = [], $dataName = '')
     {
         parent::__construct($name, $data, $dataName);
-        $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '\..\src');
+        $dotenv = Dotenv::createImmutable(__DIR__ . '\..\src');
         $dotenv->load();
     }
 
