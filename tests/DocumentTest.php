ab71955cbc4869406705cacec4ed2de6d63ae8e1
Vinicius Dutra <dutra_morais@hotmail.com>
2020-03-08 17:37:34 -0300

    test .env

--- tests/DocumentTest.php
+++ tests/DocumentTest.php
@@ -14,7 +14,7 @@ class DocumentTest extends PHPUnit\Framework\TestCase
      */
     public function testListAll()
     {
-        $data = json_decode((new vinicinbgs\Autentique\Documents(getenv('AUTENTIQUE_TOKEN')))->listAll(), true);
+        $data = json_decode((new vinicinbgs\Autentique\Documents('1AAA95DCA22EAD45836A01A561BCE34B36DEF038CCC59B07A04670305F4834A6'))->listAll(), true);
 
         $this->assertArrayHasKey('data', $data);
     }
