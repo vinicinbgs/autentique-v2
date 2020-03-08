3dbe2a78bfb3092e5480cce622aef3ec87207f9a
Vinicius Dutra <dutra_morais@hotmail.com>
2020-03-08 20:49:50 -0300

    ENVIROMENT VARIABLES

--- tests/DocumentTest.php
+++ tests/DocumentTest.php
@@ -5,8 +5,8 @@ class DocumentTest extends PHPUnit\Framework\TestCase
     public function __construct(?string $name = null, array $data = [], $dataName = '')
     {
         parent::__construct($name, $data, $dataName);
-        //$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '\..\src\.env');
-        //$dotenv->load();
+        $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../src');
+        $dotenv->load();
     }
 
     /**
@@ -14,7 +14,7 @@ class DocumentTest extends PHPUnit\Framework\TestCase
      */
     public function testListAll()
     {
-        $data = json_decode((new vinicinbgs\Autentique\Documents('1AAA95DCA22EAD45836A01A561BCE34B36DEF038CCC59B07A04670305F4834A6'))->listAll(), true);
+        $data = json_decode((new vinicinbgs\Autentique\Documents(getenv('AUTENTIQUE_TOKEN')))->listAll(), true);
 
         $this->assertArrayHasKey('data', $data);
     }
