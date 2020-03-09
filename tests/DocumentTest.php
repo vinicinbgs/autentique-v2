f6596110572874dcf0760f4ed369cb39d36fe5fe
Vinicius Dutra <dutra_morais@hotmail.com>
2020-03-08 21:47:00 -0300

    test codeclimate

--- tests/DocumentTest.php
+++ tests/DocumentTest.php
@@ -5,8 +5,8 @@ class DocumentTest extends PHPUnit\Framework\TestCase
     public function __construct(?string $name = null, array $data = [], $dataName = '')
     {
         parent::__construct($name, $data, $dataName);
-        $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../src');
-        $dotenv->load();
+        //$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../src');
+        //$dotenv->load();
     }
 
     /**
